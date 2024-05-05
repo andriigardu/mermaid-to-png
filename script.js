693768481fc62ea89bb34c3b2a51ff43dfc14213
andriigardu <122264568+andriigardu@users.noreply.github.com>
2024-05-05 16:33:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ mermaid.initialize({ startOnLoad: false });
 
 function generateChart() {
     const input = document.getElementById('mermaidInput').value;
-    // Use a hidden div to initially render the SVG
-    mermaid.mermaidAPI.render('generatedMermaid', input, function(svgCode, bindFunctions) {
-        document.getElementById('mermaidTempRender').innerHTML = svgCode;
+    // Make sure the temporary rendering element is ready and empty
+    const renderDiv = document.getElementById('mermaidTempRender');
+    renderDiv.innerHTML = '';
+    mermaid.mermaidAPI.render('generatedMermaid', input, (svgCode) => {
+        renderDiv.innerHTML = svgCode;
         convertSVGToPNG(document.getElementById('generatedMermaid'));
     });
 }
@@ -13,39 +15,13 @@ function convertSVGToPNG(svgElement) {
     const svgData = new XMLSerializer().serializeToString(svgElement);
     const canvas = document.createElement('canvas');
     const ctx = canvas.getContext('2d');
-    const svgSize = svgElement.getBoundingClientRect();
-    canvas.width = svgSize.width;
-    canvas.height = svgSize.height;
-
+    canvas.width = svgElement.clientWidth;
+    canvas.height = svgElement.clientHeight;
     const img = new Image();
     img.onload = function() {
         ctx.drawImage(img, 0, 0);
         const pngImg = canvas.toDataURL("image/png");
-        const outputImg = document.createElement('img');
-        outputImg.src = pngImg;
-        document.getElementById('mermaidOutput').innerHTML = '';
-        document.getElementById('mermaidOutput').appendChild(outputImg);
-        copyImageToClipboard(pngImg);
+        document.getElementById('mermaidOutput').innerHTML = `<img src="${pngImg}" />`;
     };
     img.src = 'data:image/svg+xml;base64,' + btoa(unescape(encodeURIComponent(svgData)));
-}
-
-function copyImageToClipboard(dataUrl) {
-    const img = new Image();
-    img.src = dataUrl;
-    const canvas = document.createElement('canvas');
-    canvas.width = img.width;
-    canvas.height = img.height;
-    const ctx = canvas.getContext('2d');
-    img.onload = () => {
-        ctx.drawImage(img, 0, 0);
-        canvas.toBlob(blob => {
-            const item = new ClipboardItem({ "image/png": blob });
-            navigator.clipboard.write([item]).then(function() {
-                console.log('Image copied to clipboard');
-            }, function(error) {
-                console.error('Error copying image: ', error);
-            });
-        });
-    };
 }
